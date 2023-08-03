6c50f02a13d91ab6ad0b840d28a44cd9cbb418f3
Matheus Felix <139878742+MFMarinho@users.noreply.github.com>
2023-08-03 14:22:03 +0000

    dia03agosto

---
+++ aula03agosto.js
@@ -0,0 +1,68 @@
+// Exercicio 01
+
+// let x = 1;
+
+// while (x <= 10){
+//     console.log(x);
+//     x++;
+// }
+
+// Exercicio 02
+
+// let y = 1
+// let x = 0;
+
+// while (x <= 100){
+//     y = y + x;
+//     x++;
+// }
+
+// console.log(y);
+
+// Exercicio 03
+
+// let x = 1; 
+// let pares = "";
+
+// while (x <= 50) {
+//     if (x % 2 === 0) {
+//         pares += x+ ", ";
+//     }
+//     x++;
+// }
+// console.log(pares);
+
+// EXERCICIO 04 //
+
+// let x = 1;
+
+// while (x <= 1000){
+//     x = x * 2;
+// }
+//  console.log(x);
+
+// EXERCICIO 05 //
+
+// let divisores;
+// let x = 10;
+// let count = 1;
+// let primos;
+
+// while (count <= x){
+//     if (x % count != 0){
+//         primos = count;
+//     }
+//     count++;
+// }
+
+// console.log(primos);
+
+// EXERCICIO 06 //
+
+// let senha = "1234";
+// let senhateste;
+
+// while (senha != senhateste){
+//     senhateste = prompt("Digite a senha?")
+//     console.log("Senha Invalida")
+// }
