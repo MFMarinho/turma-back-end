f2f2b104930b9bdd72904d4ef7bcc0fa309bc4ef
Matheus Felix <139878742+MFMarinho@users.noreply.github.com>
2023-08-08 14:19:42 +0000

    aula08agosto

---
+++ aula08agosto.js
@@ -0,0 +1,68 @@
+// Demonstração de função com parâmetros
+
+// function exibirMensagem(nome, dia, mes){
+//     console.log("Bom dia " + nome + "! Hoje é dia " + dia + " de " + mes + ".");
+// }
+
+// exibirMensagem("Matheus", "08", "agosto");
+
+// function exibirSaudacao(nome, dia, mes){
+//     const mensagem = `Bom dia ${nome}! Hoje é dia ${dia} de ${mes}.`;
+//     console.log(mensagem);
+// }
+
+// exibirSaudacao("Matheus", "08", "agosto");
+
+
+// EXERCICIO 01 
+
+// function exibirMensagem(){
+//     console.log("Olá mundo!");
+// }
+
+// exibirMensagem();
+
+
+// EXERCICIO 02
+
+// function dobro(num){
+//     const mensagem = `${num*2}`;
+//     console.log(mensagem);
+// }
+
+// dobro("2");
+
+// EXERCICIO 03
+
+// function mostrarNumeros(inicio, fim){
+//     while (inicio <= fim) {
+//         if (inicio % 2 == 0) {
+//           console.log(inicio); 
+//         }
+//         inicio++;
+//     }
+// }
+
+// mostrarNumeros("1","10");
+
+// EXERCICIO 04
+
+// function verificarPar(num){
+//     if (num % 2 === 0){
+//         console.log (`O número ${num} é par`);
+//     } else {
+//         console.log(`O número ${num} é impar`);
+//     }
+// }
+
+// verificarPar("3");
+
+// EXERCICIO 05
+
+let array = [1,2,3,4,5];
+
+function imprimirLista(lista){
+    console.log(lista);
+}
+
+imprimirLista(array);
