f1fcc69f4c31fde3b2faf1991c6f7ecbdc2f8caa
Matheus Felix <139878742+MFMarinho@users.noreply.github.com>
2023-08-17 14:29:41 +0000

    aula17agosto

---
+++ aula17agosto.js
@@ -0,0 +1,31 @@
+const readline = require('readline-sync'); // requerimento da biblioteca
+
+// Exercicio 01
+
+// let numero = readline.questionInt("Insira um número: ");
+// numero = numero/0;
+//
+// try {
+//     if (numero === Infinity){
+//         throw "Valor negado" ;
+//     }
+// } catch (erro){
+//     console.log("[Erro]: Divisão por zero não permitida!");
+// }
+
+// EXERCICIO 02
+
+
+// try{
+//     let numero = readline.question("Insira seu número: ");
+//     numero = Number.parseInt(numero);
+//     if (Number.isNaN(numero)){
+//         throw "erro";
+//     }
+
+// } catch (erro){
+//     console.log("Conversão inválida");
+// }
+
+// EXERCICIO 03
+
