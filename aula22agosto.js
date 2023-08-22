000ea00642666cf1dae37359bf31430c13663a34
Matheus Felix <139878742+MFMarinho@users.noreply.github.com>
2023-08-22 14:23:18 +0000

    aula22agosto

---
+++ aula22agosto.js
@@ -0,0 +1,55 @@
+// EXERCICIO 01
+
+// const numeros = [3, 7, 2, 9, 5];
+    
+// for (let elemento of numeros) {
+//     console.log(elemento);
+// }
+
+// EXERCICIO 02
+
+// let lista = {a: 10, b: 20, c: 30, d: 40};
+// let soma = 0;
+
+// for (let i in lista) {
+//     soma += lista[i];
+// }
+
+// console.log(soma);
+
+// EXERCICIO 03
+
+// let lista = [12, 5, 8, 21, 16, 7];
+// let par =[];
+
+// for (let i = 0; i < lista.length; i++){
+//     if (lista[i] % 2 === 0){
+//         par.push(lista[i]);
+//     }
+// }
+
+// console.log(par);
+
+// EXERCICIO 04
+
+// const estudantes = { alice: 18, bob: 20, carol: 19, david: 21 };
+
+// for (let i in estudantes){
+//     if (estudantes[i] === 19){
+//         console.log(`Nome: ${i} Idade: ${estudantes[i]}`);
+//     }
+// }
+
+// EXERCICIO 05
+
+// const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10] ];
+// let soma = 0;
+
+// for (let i of pontos){
+//     for (let x of i){
+//         soma += x;
+//     }
+
+// }
+
+// console.log(soma);
